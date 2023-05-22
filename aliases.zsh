2343e6fa9b265a97924b819dfe53577a02674517
Andrew Voynov <andrewvoynov.b@gmail.com>
2023-05-22 19:58:29 +0300

    Added aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+# General shortcuts
+alias cdz='cd $ZHOME'
+alias zali='(cdz; \nvim aliases.zsh)'
+alias zconf='(cdz; \nvim .zshrc)'
+alias conf=zconf
+alias zhi='(cdz; \nvim .history.zsh)'
+alias zr='exec zsh'
+
+# Sourcing
+alias zsrc='source $ZHOME/aliases.zsh; source $ZHOME/.zshrc'
+
+# agkozak/zsh-z
+alias fa='f --add'
